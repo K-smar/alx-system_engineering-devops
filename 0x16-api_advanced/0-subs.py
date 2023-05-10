676b0f304943d661758b827c1f544d3894b4297b
K-smar <jessicayekini0@gmail.com>
2023-05-10 12:35:36 +0000

    fixed task0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,13 +12,13 @@ def number_of_subscribers(subreddit):
     """returns number of total subscribers"""
     url = ("https://api.reddit.com/r/{}/about".format(subreddit))
     headers = {'User-Agent': 'CustomClient/1.0'}
-    response = requests.get(url, headers, allow_redirects=False)
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code != 200:
-	return (0)
+        return (0)
     response = response.json()
     if 'data' in response:
-	return (response.get('data').get('subscribers'))
+        return (response.get('data').get('subscribers'))
 
     else:
-	return (0)
+        return (0)
